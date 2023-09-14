5f4387d1e720892f56b46a2d48677e7b3afd197d
evgeny <zhenya.leshhinskij@gmail.com>
2023-09-14 21:24:53 +0300

    initial project

---
+++ sitewomen/women/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, register_converter
+from . import views
+from . import converters
+
+
+
+register_converter(converters.FourDigitYearConverter, 'year4')
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('addpage/', views.add_page, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
+    path('category/<int:cat_id>/', views.show_category, name='category'),
+
+]
