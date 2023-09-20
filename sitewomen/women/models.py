204bb086badb05da4fbeeea900249d4965de9173
evgeny <zhenya.leshhinskij@gmail.com>
2023-09-20 11:46:32 +0300

    choices

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,14 +1,31 @@
 from django.db import models
+from django.db.models.query import QuerySet
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
 
 
 class Women(models.Model):
 
+    class Status(models.IntegerChoices):
+
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     
+    objects = models.Manager()
+    published = PublishedManager()
+
 
     def __str__(self):
         return self.title
@@ -20,3 +37,7 @@ class Women(models.Model):
         indexes = [
             models.Index(fields=['-time_create']),
         ]
+
+    
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug': self.slug})
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
+from .models import Women
 
 
 menu = [
@@ -28,9 +29,11 @@ cats_db = [
 
 
 def index(request):
+    posts = Women.published.all()
+    
     data = {'title': 'Главная страница',
             'menu': menu,
-            'posts': data_db,
+            'posts': posts,
             'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=data)
@@ -44,8 +47,16 @@ def about(request):
     return render(request, 'women/about.html', data)
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отображение статьи с id = {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+    return render(request, 'women/post.html', data)
 
 
 def add_page(request):
