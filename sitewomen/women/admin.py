1082d02e498155ca38827c6cc5470a8debaa74be
evgeny <zhenya.leshhinskij@gmail.com>
2024-02-19 17:36:35 +0300

    admin panel

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,5 +1,54 @@
-from django.contrib import admin
-from .models import Women
+from django.contrib import admin, messages
+from .models import Women, Category
 
 
-admin.site.register(Women)
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщин'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return[
+            ('married', 'Замужем'),
+            ('single', 'Не замужем'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        if self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    fields = ('title',  'slug', 'content', 'cat', 'husband', 'tags')
+    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title',)
+    filter_horizontal = ('tags',)
+    ordering = ('time_create', 'title')
+    prepopulated_fields = {'slug': ('title', )}
+    list_editable = ('is_published',)
+    list_per_page = 5
+    actions = ('set_published', 'set_draft')
+    search_fields = ('title__startswith', 'cat__name')
+    list_filter = (MarriedFilter,'cat__name', 'is_published')
+
+    @admin.display(description='Краткое описание', ordering='content')
+    def brief_info(self, women: Women):
+        return f'Описание {len(women.content)} символов'
+
+    @admin.action(description='Опубликовать выбранные записи')
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f'Изменено {count} записей.')
+
+    @admin.action(description='Снять с публикации выбранные записи')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f' {count} записей снято с публикации.', messages.WARNING)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
