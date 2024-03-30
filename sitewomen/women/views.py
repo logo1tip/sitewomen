f59cd304e468c4364d9b3769ab3a6f034909ae99
evgeny <zhenya.leshhinskij@gmail.com>
2024-03-30 14:09:28 +0300

    createview, updateview

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -7,7 +7,8 @@ from django.urls import reverse, reverse_lazy
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView, FormView
+from django.views.generic import (TemplateView, ListView, DetailView,
+                                  FormView, CreateView, UpdateView)
 
 from .models import Women, Category, TagPost, UploadFiles
 from .forms import AddPostForm, UploadFileForm
@@ -125,19 +126,28 @@ class ShowPost(DetailView):
 #     return render(request, 'women/addpage.html', data)
 
 
-class AddPage(FormView):
+class AddPage(CreateView):
     form_class = AddPostForm
+    # model = Women
+    # fields = '__all__'
     template_name = 'women/addpage.html'
-    success_url = reverse_lazy('home')
-
+    # success_url = reverse_lazy('home')
     extra_context = {
         'menu': menu, 
        'title': 'Добавление статьи',    
-    } 
+    }
 
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+
+class UpdatePage(UpdateView):
+    model = Women
+    fields = ['title', 'content', 'photo', 'is_published', 'cat']
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+    extra_context = {
+        'menu': menu,
+        'title': 'Добавление статьи',
+    }
+
 
 # class AddPage(View):
 
